1627020aad16d630faa8c98181b569955bd6b03a
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-05-29 23:16:48 -0700

    Set up models and Schema

---
+++ models/Message.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, minLength: 3, maxLength: 50 },
+  text: { type: String, required: true, minLength: 1, maxLength: 560 },
+  timestamp: { type: Date, required: true },
+});
+
+MessageSchema.virtual('url').get(function () {
+  return `/message/${this._id}`;
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, minLength: 2, maxLength: 50 },
+  last_name: { type: String, required: true, minLength: 2, maxLength: 50 },
+  userSchemaname: { type: String, required: true, minLength: 3, maxLength: 50 },
+  password: { type: String, required: true, minLength: 8, maxLeegth: 50 },
+});
+
+UserSchema.virtual('url').get(function () {
+  return `/user/${this._id}`;
+});
+
+UserSchema.virtual('family_name').get(function () {
+  if (!this.first_name) {
+    const first_name = '';
+  } else {
+    const first_name = this.first_name;
+  }
+  if (!this.last_name) {
+    const last_name = '';
+  } else {
+    const last_name = ` ${this.last_name}`;
+  }
+
+  return first_name + last_name;
+});
+
+module.exports = mongoose.model('User', UserSchema);
