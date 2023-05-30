cc2f18a3bf6382475f8fb63f6d13c1fa50e7b132
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-05-30 03:40:09 -0700

    Add more packages and set up bcrypt

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+
+const User = require('./models/User');
 
 const app = express();
 
@@ -15,6 +18,49 @@ main().catch((err) => console.error(err));
 
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'pug');
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = User.find({ name: username }).exec();
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  })
+);
+
+app.post(
+  'log-in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/log-in',
+  })
+);
 
 app.use(session({ secret: 'member', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
